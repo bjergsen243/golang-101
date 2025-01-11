c383f6f394007e5964b86c09a0a96013293a12b4
Son Nguyen Thanh <thanhsondeptrai243@gmail.com>
2025-01-11 16:29:11 +0700

    update function and closures

--- examples/closures/main.go
+++ examples/closures/main.go
@@ -26,4 +26,21 @@ func main() {
 
 	fmt.Println("People after sort:", people)
 
+	// closure example
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+
+}
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
 }
--- examples/functions/main.go
+++ examples/functions/main.go
@@ -3,9 +3,48 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("annotated function")
+	anonymousFunc()
+
+	fmt.Println("plus function")
+	resPlus := plus(1, 2)
+	fmt.Println("1 + 2 =", resPlus)
+
+	fmt.Println("return multiple values")
+	a, b := returnMultipleValues()
+	fmt.Println("a:", a, "b:", b)
+
+	fmt.Println("variadic function")
+	variadicFunc(1, 2)
+	variadicFunc(1, 2, 3)
+
+	nums := []int{1, 2, 3, 4}
+	variadicFunc(nums...)
+}
+
+// annonymous function is useful when you want to define a function inline
+// without having to name it
+func anonymousFunc() {
 	for i := 0; i < 5; i++ {
 		func(j int) {
 			fmt.Println("printing", j, "from inside the anonymous function")
 		}(i)
 	}
+}
+
+func plus(a, b int) int {
+	return a + b
+}
+
+func returnMultipleValues() (int, int) {
+	return 3, 7
+}
+
+func variadicFunc(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println("total is:", total)
 }
